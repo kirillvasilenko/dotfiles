d42cc7f28f22ac5c3f6230eaf4a533eab29beaf7
Kirill Vasilenko <kir-vasilenko@ybd.tech>
2026-04-12 08:48:34 +0000

    arrange nvim configs

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,16 @@
+-- Line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Indentation
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- Search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- UI
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
---
+++ nvim/lua/config/telescope.lua
@@ -0,0 +1,45 @@
+
+-- Telescope
+local telescope = require("telescope")
+local builtin = require("telescope.builtin")
+
+-- Setup
+telescope.setup({
+  defaults = {
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+    },
+  },
+})
+
+-- In the search window you can ctrl+v/x/t - open the file in a vertical/horisontal split/new tab
+vim.keymap.set("n", "<leader>ff", builtin.find_files)
+vim.keymap.set("n", "<leader>fb", builtin.buffers)
+vim.keymap.set("n", "<leader>fr", builtin.resume)
+-- find the word under the cursor
+vim.keymap.set("n", "<leader>fw", builtin.grep_string)
+vim.keymap.set("n", "<leader>fg", builtin.live_grep)
+-- find all
+vim.keymap.set("n", "<leader>fa", function()
+  builtin.live_grep({
+    additional_args = function()
+      return { "--hidden" }
+    end,
+    prompt_title = "Live Grep Hidden",
+  })
+end)
+-- find in the current folder
+vim.keymap.set("n", "<leader>fd", function()
+  builtin.live_grep({
+    cwd = vim.fn.expand("%:p:h"),
+    prompt_title = "Live Grep Current Dir",
+  })
+end)
+vim.keymap.set("n", "<leader>/", builtin.current_buffer_fuzzy_find)
+
