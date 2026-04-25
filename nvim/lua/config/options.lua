1dce537b97832f412d3d2947a0d220d326a8608b
Kirill Vasilenko <kirill.amurskiy@gmail.com>
2026-04-25 12:09:44 +0300

    support clipboard

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -14,3 +14,18 @@ vim.opt.smartcase = true
 -- UI
 vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
+
+-- Clipboard
+vim.opt.clipboard = "unnamedplus"
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
