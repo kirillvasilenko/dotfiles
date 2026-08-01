97d8d879e9f56ca2102cd5015b3532bcc417185f
Kirill Vasilenko <kir-vasilenko@ybd.tech>
2026-08-01 08:29:30 +0000

    add bufferline

---
+++ nvim/lua/kir/plugins/bufferline.lua
@@ -0,0 +1,12 @@
+return {
+  "akinsho/bufferline.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  version = "*",
+  opts = {
+    options = {
+      mode = "tabs",
+      -- does not show what tab is active with the default colorscheme
+      -- separator_style = "slant",
+    },
+  },
+}
