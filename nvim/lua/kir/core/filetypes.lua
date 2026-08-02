0e5fcf90ca0def96a7eba76706d5536c105b888f
Kirill Vasilenko <kir-vasilenko@ybd.tech>
2026-08-02 15:35:08 +0000

    proper tabs for markdown

--- nvim/lua/kir/core/filetypes.lua
+++ nvim/lua/kir/core/filetypes.lua
@@ -4,3 +4,12 @@ vim.filetype.add({
     h = "cpp",
   },
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.bo.tabstop = 2
+    vim.bo.shiftwidth = 2
+    vim.bo.expandtab = true
+  end,
+})
