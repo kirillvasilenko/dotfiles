005189afe522072f5bd44d96865bce2d931d513f
Kirill Vasilenko <kir-vasilenko@ybd.tech>
2026-08-02 10:26:28 +0000

    add formatting

--- nvim/lua/kir/plugins/lsp/mason.lua
+++ nvim/lua/kir/plugins/lsp/mason.lua
@@ -4,6 +4,7 @@ return {
     "neovim/nvim-lspconfig",
     "mason-org/mason-lspconfig.nvim",
     "hrsh7th/cmp-nvim-lsp",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
   config = function()
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
@@ -44,6 +45,16 @@ return {
         "pyright",
         "clangd",
         "sqls",
+      },
+    })
+
+    require("mason-tool-installer").setup({
+      ensure_installed = {
+        "prettier", -- prettier formatter
+        "stylua", -- lua formatter
+        "isort", -- python formatter
+        "black", -- python formatter
+        "clang-format", -- c/c++ formatter
       },
     })
   end,
